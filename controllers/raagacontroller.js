3337d4a7e7cda520f1c758d41d783dcdea197085
Rishi <rishi@Rishikeshs-MacBook-Air.local>
2019-03-02 17:25:46 +0530

    Added pagination support for Raagas

--- controllers/raagacontroller.js
+++ controllers/raagacontroller.js
@@ -1,6 +1,6 @@
 var Note = require("../models/note")
 var Raaga = require("../models/raaga")
-
+require('mongoose-paginate-v2');
 exports.addraaga = function (req, res, next) {
 var multiraagas = []
 for(i=0;i<req.body.length;i++) {
@@ -40,28 +40,50 @@ exports.getraaga = function(req, res, next){
                select: { languages: { $elemMatch: { language: req.language } }}})
 }
 
+
 exports.getraagalist = function(req, res, next){
-  var offset = req.query.offset
-  if(offset === undefined)
-     offset = 0
-  console.log(offset)
-  Raaga.find({}, {_id:0, 'parent':1, 'melakarthanum':1,'origin':1, languages: {$elemMatch: {language: req.language}}}, function(err, raagas) {
-    if (err)
-      return next(err);
-    else if (raagas === null)
-      return next()
-    else
-    {
-      console.log(raagas)
-      res.status(200).send(raagas);
-    }
-  }).populate({path: 'aarohanam',
-               select: { languages: { $elemMatch: { language: req.language }}}}).
-     populate({path: 'avarohanam',
-               select: { languages: { $elemMatch: { language: req.language } }}}).
-     limit(5).
-     skip(offset*5)
+  var pagenum =  parseInt(req.query.page, 10)
+  if(Number.isNaN(pagenum))
+     pagenum = 1
+  const myCustomLabels = {
+    totalDocs: 'raagacount',
+    docs: 'raagas',
+    limit: 'pagesize',
+    page: 'currentpage',
+    nextPage: 'next',
+    prevPage: 'prev',
+    totalPages: 'pagecount',
+    pagingCounter: 'slNo'
+};
+
+var options = {
+    limit: 5,
+    page: pagenum,
+    select: {'parent':1, 'melakarthanum':1,'origin':1, languages: {$elemMatch: {language: req.language}}},
+    populate: [{  path: 'aarohanam',
+                  select: { languages: { $elemMatch: { language: req.language }}}
+               },
+               {  path: 'avarohanam',
+                  select: { languages: { $elemMatch: { language: req.language }}}
+               }],
+    customLabels: myCustomLabels
+};
+
+  Raaga.paginate({}, options, function(err, raagaresult) {
+   if (err)
+     return next(err);
+   else if (raagaresult.raagas === null)
+     return next()
+   else
+   {
+     res.status(200).send(raagaresult);
+   }
+ })
+
 }
+
+
+
 
 exports.updateraaga = function(req, res, next){
   Raaga.findByIdAndUpdate(req.params.id, req.body, {new:true}, function(err, raaga) {
@@ -90,22 +112,3 @@ exports.deleteraaga = function(req, res, next){
     }
   })
 }
-
-/*exports.getraagalanguage = function(req, res, next){
-  Raaga.findById(req.params.id, {languages: {$elemMatch: {language: req.params.language}}}, {"languages.$": 1}, function(err, raaga) {
-    if (err)
-      return next(err);
-    else if (raaga === null || raaga.languages.length === 0)
-      return next()
-    else
-    {
-      console.log(raaga.languages.length)
-      res.status(200).send(raaga);
-    }
-  }).populate({path: 'aarohanam',
-               match: { "languages.language": req.params.language},
-               select: {"languages.language.symbol"}
-             }).
-  populate('avarohanam');*/
-
-//}
--- models/raaga.js
+++ models/raaga.js
@@ -1,5 +1,6 @@
 /* raaga.js */
 var mongoose = require('mongoose');
+var mongoosepaginate = require('mongoose-paginate-v2')
 var Schema = mongoose.Schema,
 ObjectId = Schema.ObjectId;
 var raagaSchema = new mongoose.Schema({
@@ -16,6 +17,6 @@ var raagaSchema = new mongoose.Schema({
   aarohanam: [{ type: Schema.Types.Mixed, ref: 'Note'}],
   avarohanam: [{ type: Schema.Types.Mixed, ref: 'Note'}]
 });
-
+raagaSchema.plugin(mongoosepaginate)
 mongoose.model('Raaga', raagaSchema);
 module.exports = mongoose.model('Raaga');
